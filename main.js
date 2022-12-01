1511471ec852f2db06ccaf8a45e6a123aeb958e2
Thomas Antoni <antonithomas06@gmail.com>
2022-12-01 22:40:16 +0100

    adventcalendar first commit

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ const calendarContainer = document.querySelector(".container");
 const calendarDays = 24;
 
 const openDoor = (path, event) => {
+    let today = new Date().getDate();
+    let calendarDate = event.target.parentNode.getAttribute("data-datecheck");
+    //console.log("Heute ist " + today.getDate());
+    console.log(event.target.parentNode.getAttribute("data-datecheck"));
+    console.log(today);
+    if(today >= calendarDate){
     event.target.parentNode.style.backgroundImage = `url(${path})`;
     event.target.style.opacity = "0";
     event.target.style.backgroundColor = "#521751";
+    }
+    else{
+        event.target.style.backgroundColor = "#F3f321";
+        event.target.innerHTML = "TO SOON";
+    }
 }
 
 const createCalendar = () => {
@@ -20,10 +31,11 @@ const createCalendar = () => {
 
         calendarDoorText.classList.add("text");
         calendarDoorText.innerHTML = i + 1;
+        calendarDoor.setAttribute("data-datecheck",i+1);
         calendarDoor.appendChild(calendarDoorText);
 
-        courseNumber = i + 1;
-        let coursePath = `./courses/course-${courseNumber}.jpg`;
+        imgNumber = i + 1;
+        let coursePath = `./courses/Fenster${imgNumber}.jpg`;
 
         calendarDoorText.addEventListener("click", openDoor.bind(null,  coursePath));
     }
